2dd0f69f3b426269dc3812583431de0d15acaa44
g-yz <gladys.chy5@gmail.com>
2022-10-22 00:14:16 -0500

    change styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,25 @@
 import LoginForm from '../features/auth/LoginForm';
+import styles from './Login.module.css';
 import { Box, Card, Stack, Typography } from '@mui/material';
 
 export default function Login() {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" bgcolor="primary.light" minHeight="100vh">
-      <Card>
-        <Box p={4} >
+    <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <div className={styles.wrapper}>
+        <Box p={4} sx={{ color: 'text.secondary' }}>
           <Stack direction="row" alignItems="center" sx={{ mb: 4 }}>
-            <Box sx={{ flexGrow: 1 }}  >
-              <Typography variant="h4" gutterBottom align="center">
+            <Box sx={{ flexGrow: 1 }}>
+              <Typography variant="h4" gutterBottom align="center" color="white">
                 Rocket App
               </Typography>
-              <Typography sx={{ color: 'text.secondary' }} align="center">Enter your details below.</Typography>
+              <Typography sx={{ color: 'text.secondary' }} align="center">
+                Enter your details below.
+              </Typography>
             </Box>
           </Stack>
           <LoginForm />
         </Box>
-      </Card>
+      </div>
     </Box>
   );
 }
