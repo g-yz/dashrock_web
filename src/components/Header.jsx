93a65ad6bb6948e61f7ada15468f1f19b1f96ba3
g-yz <gladys.chy5@gmail.com>
2022-10-22 12:32:57 -0500

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -76,7 +76,7 @@ const Header = () => {
               aria-expanded={open ? 'true' : undefined}
             >
               <Avatar sx={{ width: 32, height: 32, bgcolor: '#0094FF' }}>
-                {currentUser?.full_name.length > 0 ? currentUser?.full_name[0] : 'M'}
+                {currentUser?.full_name?.length > 0 ? currentUser?.full_name[0] : 'M'}
               </Avatar>
             </IconButton>
           </Tooltip>
@@ -94,7 +94,7 @@ const Header = () => {
       >
         <MenuItem>
           <Avatar sx={{ bgcolor: '#0094FF' }}>
-            {currentUser?.full_name.length > 0 ? currentUser?.full_name[0] : 'M'}
+            {currentUser?.full_name?.length > 0 ? currentUser?.full_name[0] : 'M'}
           </Avatar>
           {currentUser?.full_name}
         </MenuItem>
