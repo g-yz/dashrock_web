81a4f2ade023ed11ab73553e82f786266eea63ba
g-yz <gladys.chy5@gmail.com>
2022-10-22 23:47:21 -0500

    update url

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,10 +13,25 @@ const Router = () => {
   const externalDashboard =
     'https://datastudio.google.com/embed/reporting/a1a80867-df7c-4d82-bc35-396b068a78fb/page/8mn5C';
 
-  const getRouteBygroup = group => {
+  const getRouteBygroup2 = group => {
+    const idGroup = group.replace('Vip  ', '');
     const api_base_report = 'https://datastudio.google.com/embed/reporting';
-    const api_report = '/64d990aa-6f13-459d-812f-d936986d2c28/page/8mn5C?';
-    return api_base_report + api_report + 'params=%7B%22ds8.userlogueadogrupo%22:%22' + group + '%22%7D';
+    const api_report = '/64d990aa-6f13-459d-812f-d936986d2c28/page/p_3pimijuuzc?';
+    return api_base_report + api_report + 'params=%7B%22ds8.userlogueadogrupo%22:%22' + idGroup + '%22%7D';
+  };
+
+  const getRouteBygroup = group => {
+    // const idGroup = group.replace('Vip  ', '');
+    const idGroup = group[group?.length - 1];
+    let params = {
+      'ds9.userloggroid': idGroup,
+    };
+    let paramsAsString = JSON.stringify(params);
+    let encodedParams = encodeURIComponent(paramsAsString);
+
+    const api_base_report = 'https://datastudio.google.com/embed/reporting';
+    const api_report = '/64d990aa-6f13-459d-812f-d936986d2c28/page/p_3pimijuuzc?';
+    return api_base_report + api_report + 'params=' + encodedParams;
   };
 
   const dispatch = useDispatch();
